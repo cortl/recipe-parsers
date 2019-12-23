20eecae4594ba6694b70eee862638ee66495061b
Cortlan Bainbridge <cortlan.bainbridge@gmail.com>
2019-12-23 15:09:17 -0600

    future proofing the parsing

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const fs = require('fs');
-
+const URL = require('url-parse');
 const readline = require('readline').createInterface({
     input: process.stdin,
     output: process.stdout
 });
-
 const budgetbytes = require('./parsers/budgetbytes');
+
+const PARSERS = {
+    'www.budgetbytes.com': budgetbytes.parse
+}
 
 const main = async () => {
     readline.question('what recipe url would you like to parse?\n', async (url) => {
-        await budgetbytes.parse(url)
+        await PARSERS[URL(url).hostname](url)
             .then(recipe => {
                 const fileName = `${recipe.title.toLowerCase().replace(/\s/g, '-')}.json`;
                 const location = `recipes/${fileName}`
