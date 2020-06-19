6944368f6104f7af8637e241e829ab99a273d2fa
cortl <cortlan.bainbridge@gmail.com>
2020-06-19 11:23:01 -0500

    adding sourdough and complex ingredient lists

--- test.js
+++ test.js
@@ -3,17 +3,38 @@ const assert = require('assert');
 const path = require('path');
 
 const items = fs.readdirSync('recipes');
+
+const isAString = (thing) => assert.strictEqual(typeof (thing), 'string')
 items.forEach(item => {
     console.log(`Testing ${item}`);
     const recipe = JSON.parse(fs.readFileSync(`recipes/${item}`));
-    const {title, rating, slug, notes, source, image, instructions} = recipe;
+    const { title, rating, slug, notes, source, image, instructions, ingredients, ...unused } = recipe;
 
-    assert.strictEqual(typeof(title), 'string');
-    assert.strictEqual(typeof(rating), 'number');
-    assert.strictEqual(typeof(slug), 'string');
-    assert.strictEqual(typeof(source), 'string');
-    assert.strictEqual(typeof(instructions), 'object');
-    assert.strictEqual(typeof(notes), 'object');
+    assert.ok(!Object.keys(unused).length);
+
+    assert.strictEqual(typeof (title), 'string');
+    assert.strictEqual(typeof (rating), 'number');
+    assert.strictEqual(typeof (slug), 'string');
+    assert.strictEqual(typeof (source), 'string');
+
+    assert.ok(Array.isArray(instructions));
+    instructions.forEach(isAString);
+
+    assert.ok(Array.isArray(notes));
+    notes.forEach(isAString)
+
+    assert.ok(Array.isArray(ingredients));
+    ingredients.forEach(ingredient => {
+        if (ingredient instanceof Object) {
+            const { category, items, ...extra } = ingredient;
+ 
+            assert.ok(!Object.keys(extra).length);
+            assert.strictEqual(typeof (category), 'string');
+            assert.ok(Array.isArray(items));
+        } else {
+            isAString(ingredient)
+        }
+    })
 
     if (image) {
         const imagePath = path.normalize(`recipes/${image}`);
