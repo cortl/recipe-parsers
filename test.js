51861b02e395bb1a753ce6835050759b449ee2d6
cortl <cortlan.bainbridge@gmail.com>
2020-06-19 12:04:59 -0500

    adding additional fields to recipes

--- test.js
+++ test.js
@@ -8,7 +8,11 @@ const isAString = (thing) => assert.strictEqual(typeof (thing), 'string')
 items.forEach(item => {
     console.log(`Testing ${item}`);
     const recipe = JSON.parse(fs.readFileSync(`recipes/${item}`));
-    const { title, rating, slug, notes, source, image, instructions, ingredients, ...unused } = recipe;
+    const {
+        title, rating, slug, notes,
+        source, image, instructions, ingredients,
+        createdDate, ...unused
+    } = recipe;
 
     assert.ok(!Object.keys(unused).length);
 
@@ -16,6 +20,8 @@ items.forEach(item => {
     assert.strictEqual(typeof (rating), 'number');
     assert.strictEqual(typeof (slug), 'string');
     assert.strictEqual(typeof (source), 'string');
+
+    (createdDate) && assert.ok(!isNaN(new Date(createdDate).getTime()))
 
     assert.ok(Array.isArray(instructions));
     instructions.forEach(isAString);
@@ -27,7 +33,7 @@ items.forEach(item => {
     ingredients.forEach(ingredient => {
         if (ingredient instanceof Object) {
             const { category, items, ...extra } = ingredient;
- 
+
             assert.ok(!Object.keys(extra).length);
             assert.strictEqual(typeof (category), 'string');
             assert.ok(Array.isArray(items));
