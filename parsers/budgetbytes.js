9d64d398a6600d93f75b8e975c58f4ce1c83b962
cortl <cortlan.bainbridge@gmail.com>
2020-07-06 16:53:18 -0500

    updating budgetbytes site to work with categories

--- parsers/budgetbytes.js
+++ parsers/budgetbytes.js
@@ -7,16 +7,35 @@ const parse = async (source, notes, rating) => {
         .then(res => res.data)
         .then(data => cheerio.load(data));
 
-    const ingredients = $('.wprm-recipe-ingredient').map((_, element) => {
-        const select = cheerio.load(cheerio.html($(element)));
+    const grabIngredientsUnderneath = (groupSelect) => groupSelect('.wprm-recipe-ingredient')
+        .map((_, element) => {
+            const select = cheerio.load(cheerio.html($(element)));
 
-        const name = select('.wprm-recipe-ingredient-name').text();
-        const unit = select('.wprm-recipe-ingredient-unit').text();
-        const amount = select('.wprm-recipe-ingredient-amount').text();
-        const measurement = Boolean(unit) ? `${amount} ${unit}` : `${amount}`;
+            const name = select('.wprm-recipe-ingredient-name').text();
+            const unit = select('.wprm-recipe-ingredient-unit').text();
+            const amount = select('.wprm-recipe-ingredient-amount').text();
+            const measurement = Boolean(unit) ? `${amount} ${unit}` : `${amount}`;
 
-        return `${measurement} ${name}`
-    }).get()
+            return `${measurement} ${name}`
+        }).get();
+
+    const isGrouped = Boolean($('.wprm-recipe-group-name').length)
+
+    const ingredients = isGrouped
+        ? $('.wprm-recipe-ingredient-group').map((_, element) => {
+            const groupSelect = cheerio.load(cheerio.html($(element)));
+
+            let section = {
+                category: groupSelect('.wprm-recipe-ingredient-group-name').text(),
+                items: grabIngredientsUnderneath(groupSelect, element)
+            }
+            return section
+        }).get()
+        : [{
+            category: 'All',
+            items: grabIngredientsUnderneath($)
+        }]
+
     const instructions = $('.wprm-recipe-instruction-text').map((_, element) => $(element).text()).get();
     const title = $('h1').text();
     const slug = util.createSlug(title);
--- parsers/test.js
+++ parsers/test.js
@@ -1,8 +1,8 @@
-const budgetbytes = require('./pressurecookingtoday');
+const budgetbytes = require('./budgetbytes');
 
 const main = async () => {
     const rating = 8.2, notes = 'test';
-    const source = 'https://www.pressurecookingtoday.com/spinach-artichoke-dip-in-the-pressure-cooker/';
+    const source = 'https://www.budgetbytes.com/green-chile-chicken-enchiladas/';
 
     const result = await budgetbytes.parse(source, notes, rating);
     console.log(JSON.stringify(result, null, 2));
